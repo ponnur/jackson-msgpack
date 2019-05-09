5985d4e36449d0e2c31a2c347ada79536ecdff87
ponnu <ponnulingam.rangasamy@verizon.com>
2019-05-09 03:21:12 -0700

    Fixed the typo

--- src/test/scala/MsgPackSpec.scala
+++ src/test/scala/MsgPackSpec.scala
@@ -13,17 +13,17 @@ class MsgPackSpec extends FlatSpec {
     val msgpackMapper = new ObjectMapper(new MessagePackFactory)
     msgpackMapper.registerModule(DefaultScalaModule)
 
-    val stingToByteArray = (str: String) => str.split(" ").map(Integer.parseInt(_, 16).toByte)
+    val stringToByteArray = (str: String) => str.split(" ").map(Integer.parseInt(_, 16).toByte)
 
     val testValues: Array[Int] = Array(10,13)
 
     val data = "92 0a 0d" // Message pack hex val for the array [10,13]
-    val convertedValues: Array[Int] = msgpackMapper.readValue(stingToByteArray(data), classOf[Array[Int]])
+    val convertedValues: Array[Int] = msgpackMapper.readValue(stringToByteArray(data), classOf[Array[Int]])
     convertedValues.map(println(_)) //Prints 10 and 13
 
 
     val invalidData = "93 0a 0d" // Invalid Message pack hex val for the array [10,13]. Array size is 3 instead of 2
-    val result: Array[Int] = msgpackMapper.readValue(stingToByteArray(invalidData), classOf[Array[Int]])// java.lang.OutOfMemoryError: Java heap space
+    val result: Array[Int] = msgpackMapper.readValue(stringToByteArray(invalidData), classOf[Array[Int]])// java.lang.OutOfMemoryError: Java heap space
 
   }
 
